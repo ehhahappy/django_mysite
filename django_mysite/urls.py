6660964c5903f125a4b6f0794d5f32f5a4d16d5b
ehhahappy <mqmqmqmqmq@naver.com>
2021-02-19 00:01:11 +0900

    Add photoApp URL, templates

--- django_mysite/urls.py
+++ django_mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django_mysite.views import HomeView
 #from bookmark.views import BookmarkLV, BookmarkDV
 
@@ -24,8 +27,9 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
+    path('photo/', include('photo.urls')),
 
     # class-based views
     # path('bookmark/', BookmarkLV.as_view(), name='index'),
     # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
